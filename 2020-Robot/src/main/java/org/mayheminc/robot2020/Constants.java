a8e4c7b0244b62b18f5250218aff66a37b20f9ec
Ihedgehogs <58241515+Ihedgehogs@users.noreply.github.com>
2020-01-17 22:04:48 -0500

    copied the joystick deffinitions into 2020

--- 2020-Robot/src/main/java/org/mayheminc/robot2020/Constants.java
+++ 2020-Robot/src/main/java/org/mayheminc/robot2020/Constants.java
@@ -18,6 +18,34 @@ package org.mayheminc.robot2020;
  * wherever the constants are needed, to reduce verbosity.
  */
 public final class Constants {
+    public final class GAMEPAD_AXIS {
+        public static final int GAMEPAD_F310_LEFT_X_AXIS = 0;
+        public static final int GAMEPAD_F310_LEFT_Y_AXIS = 1;
+        public static final int GAMEPAD_F310_LEFT_TRIGGER = 2;
+        public static final int GAMEPAD_F310_RIGHT_TRIGGER = 3;
+        public static final int GAMEPAD_F310_RIGHT_X_AXIS = 4;
+        public static final int GAMEPAD_F310_RIGHT_Y_AXIS = 5;
+    }
+
+    public final class GAMEPAD_BUTTION {
+        public static final int GAMEPAD_F310_A_BUTTON = 1;
+        public static final int GAMEPAD_F310_B_BUTTON = 2;
+        public static final int GAMEPAD_F310_X_BUTTON = 3;
+        public static final int GAMEPAD_F310_Y_BUTTON = 4;
+        public static final int GAMEPAD_F310_LEFT_BUTTON = 5;
+        public static final int GAMEPAD_F310_RIGHT_BUTTON = 6;
+        public static final int GAMEPAD_F310_BACK_BUTTON = 7;
+        public static final int GAMEPAD_F310_START_BUTTON = 8;
+        public static final int GAMEPAD_F310_LEFT_STICK_BUTTON = 9;
+        public static final int GAMEPAD_F310_RIGHT_STICK_BUTTON = 10;
+    }
+
+    public final class OPERATOR_PAD_AXIS {
+        public static final int OPERATOR_PAD_LEFT_X_AXIS = 0;
+        public static final int OPERATOR_PAD_LEFT_Y_AXIS = 1;
+        public static final int OPERATOR_PAD_RIGHT_X_AXIS = 2;
+        public static final int OPERATOR_PAD_RIGHT_Y_AXIS = 3;
+    }
 
     public final class Joysticks {
         public static final int DRIVER_GAMEPAD = 0;
--- 2020-Robot/src/main/java/org/mayheminc/robot2020/RobotContainer.java
+++ 2020-Robot/src/main/java/org/mayheminc/robot2020/RobotContainer.java
@@ -32,7 +32,72 @@ public class RobotContainer {
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
   private final Joystick DRIVER_PAD = new Joystick(Constants.Joysticks.DRIVER_GAMEPAD);
+
+  @SuppressWarnings("unused")
+  private final Button DRIVER_PAD_BUTTON_FIVE = new JoystickButton(DRIVER_PAD, 5); // Left Top Trigger
+  @SuppressWarnings("unused")
+  private final Button DRIVER_PAD_BUTTON_SIX = new JoystickButton(DRIVER_PAD, 6); // Right Top Trigger
+  @SuppressWarnings("unused")
+  private final Button DRIVER_PAD_LEFT_STICK_BUTTON = new JoystickButton(DRIVER_PAD,
+      Constants.GAMEPAD_BUTTION.GAMEPAD_F310_LEFT_STICK_BUTTON); // Left Stick Trigger
+  @SuppressWarnings("unused")
+  private final Button DRIVER_PAD_RIGHT_STICK_BUTTON = new JoystickButton(DRIVER_PAD,
+      Constants.GAMEPAD_BUTTION.GAMEPAD_F310_RIGHT_STICK_BUTTON); // Right Stick Trigger
+
+  @SuppressWarnings("unused")
+  private final JoystickPOVButton DRIVER_PAD_D_PAD_UP = new JoystickPOVButton(DRIVER_PAD, 0);
+  @SuppressWarnings("unused")
+  private final JoystickPOVButton DRIVER_PAD_D_PAD_RIGHT = new JoystickPOVButton(DRIVER_PAD, 90);
+  @SuppressWarnings("unused")
+  private final JoystickPOVButton DRIVER_PAD_D_PAD_DOWN = new JoystickPOVButton(DRIVER_PAD, 180);
+  @SuppressWarnings("unused")
+  private final JoystickPOVButton DRIVER_PAD_D_PAD_LEFT = new JoystickPOVButton(DRIVER_PAD, 270);
+
+  private final JoystickPOVButton OPERATOR_PAD_D_PAD_LEFT = new JoystickPOVButton(OPERATOR_PAD, 270);
+  private final JoystickPOVButton OPERATOR_PAD_D_PAD_RIGHT = new JoystickPOVButton(OPERATOR_PAD, 90);
+  private final JoystickPOVButton OPERATOR_PAD_D_PAD_UP = new JoystickPOVButton(OPERATOR_PAD, 0);
+  private final JoystickPOVButton OPERATOR_PAD_D_PAD_DOWN = new JoystickPOVButton(OPERATOR_PAD, 180);
+
+  private final Button CLIMB_L3_BUTTON_PAIR = new AndJoystickButton(DRIVER_PAD,
+      Constants.GAMEPAD_BUTTION.GAMEPAD_F310_Y_BUTTON, DRIVER_PAD, Constants.GAMEPAD_BUTTION.GAMEPAD_F310_LEFT_BUTTON); // Y=Yellow,
+                                                                                                                        // plus
+                                                                                                                        // left
+                                                                                                                        // top
+                                                                                                                        // trigger
+  private final Button CLIMB_L2_BUTTON_PAIR = new AndJoystickButton(DRIVER_PAD,
+      Constants.GAMEPAD_BUTTION.GAMEPAD_F310_A_BUTTON, DRIVER_PAD, Constants.GAMEPAD_BUTTION.GAMEPAD_F310_LEFT_BUTTON); // A=Green,
+                                                                                                                        // plus
+                                                                                                                        // left
+                                                                                                                        // top
+                                                                                                                        // trigger
+
+  // Driver Control Modes
+  @SuppressWarnings("unused")
+  private final Button TOGGLE_CLOSED_LOOP_MODE_BUTTON = new DisabledOnlyJoystickButton(DRIVER_PAD, 7);
+  @SuppressWarnings("unused")
+  private final Button TOGGLE_FOD_BUTTON = new DisabledOnlyJoystickButton(DRIVER_PAD, 8);
+
+  // NOTE: DRIVER_PAD_RIGHT_UPPER_TRIGGER_BUTTON is "QUICKTURN" in Drive.java - DO
+  // NOT USE HERE!!!
+  private final Button DRIVER_PAD_LEFT_UPPER_TRIGGER_BUTTON = new EnabledOnlyJoystickButton(DRIVER_PAD,
+      Constants.GAMEPAD_BUTTION.GAMEPAD_F310_LEFT_BUTTON);
+  private final JoystickAxisButton DRIVER_PAD_LEFT_LOWER_TRIGGER_BUTTON = new JoystickAxisButton(DRIVER_PAD,
+      Constants.GAMEPAD_AXIS.GAMEPAD_F310_LEFT_TRIGGER, JoystickAxisButton.POSITIVE_ONLY);
+  private final JoystickAxisButton DRIVER_PAD_RIGHT_LOWER_TRIGGER_BUTTON = new JoystickAxisButton(DRIVER_PAD,
+      Constants.GAMEPAD_AXIS.GAMEPAD_F310_RIGHT_TRIGGER, JoystickAxisButton.POSITIVE_ONLY);
+
+  @SuppressWarnings("unused")
+  private final Button DRIVER_PAD_BACK_BUTTON = new JoystickButton(DRIVER_PAD,
+      Constants.GAMEPAD_BUTTION.GAMEPAD_F310_BACK_BUTTON);
+  private final Button DRIVER_PAD_START_BUTTON = new JoystickButton(DRIVER_PAD,
+      Constants.GAMEPAD_BUTTION.GAMEPAD_F310_START_BUTTON);
+  private final Button DRIVER_PAD_GREEN_BUTTON = new JoystickButton(DRIVER_PAD, 1); // Green "A" button
+  private final Button DRIVER_PAD_RED_BUTTON = new JoystickButton(DRIVER_PAD, 2); // RED 'B" button
+  private final Button DRIVER_PAD_BLUE_BUTTON = new JoystickButton(DRIVER_PAD, 3); // BLUE 'X' button
+  private final Button DRIVER_PAD_YELLOW_BUTTON = new JoystickButton(DRIVER_PAD, 4); // YELLOW 'Y' button
+
   private final Joystick DRIVER_STICK = new Joystick(Constants.Joysticks.DRIVER_JOYSTICK);
+
   private final Button DRIVER_STICK_BUTTON_ONE_DISABLED = new DisabledOnlyJoystickButton(DRIVER_STICK, 1);
   private final Button DRIVER_STICK_BUTTON_ONE_ENABLED = new EnabledOnlyJoystickButton(DRIVER_STICK, 1);
   private final Button DRIVER_STICK_BUTTON_TWO = new DisabledOnlyJoystickButton(DRIVER_STICK, 2);
@@ -47,6 +112,37 @@ public class RobotContainer {
   private final Button DRIVER_STICK_BUTTON_ELEVEN = new DisabledOnlyJoystickButton(DRIVER_STICK, 11);
   public final int DRIVER_STICK_X_AXIS = 0;
   public final int DRIVER_STICK_Y_AXIS = 1;
+
+  public static final Joystick OPERATOR_PAD = new Joystick(Constants.Joysticks.OPERATOR_GAMEPAD);
+  private static final Button OPERATOR_PAD_BUTTON_ONE = new JoystickButton(OPERATOR_PAD, 1);
+  private static final Button OPERATOR_PAD_BUTTON_TWO = new JoystickButton(OPERATOR_PAD, 2);
+  private static final Button OPERATOR_PAD_BUTTON_THREE = new JoystickButton(OPERATOR_PAD, 3);
+  private static final Button OPERATOR_PAD_BUTTON_FOUR = new JoystickButton(OPERATOR_PAD, 4);
+  private static final Button OPERATOR_PAD_BUTTON_FIVE = new JoystickButton(OPERATOR_PAD, 5);
+  private static final Button OPERATOR_PAD_BUTTON_SIX = new JoystickButton(OPERATOR_PAD, 6);
+  private static final Button OPERATOR_PAD_BUTTON_SEVEN = new JoystickButton(OPERATOR_PAD, 7);
+  private static final Button OPERATOR_PAD_BUTTON_EIGHT = new JoystickButton(OPERATOR_PAD, 8);
+  private static final Button OPERATOR_PAD_BUTTON_NINE = new JoystickButton(OPERATOR_PAD, 9);
+  private static final Button OPERATOR_PAD_BUTTON_TEN = new JoystickButton(OPERATOR_PAD, 10);
+  @SuppressWarnings("unused")
+  private static final Button OPERATOR_PAD_BUTTON_ELEVEN = new JoystickButton(OPERATOR_PAD, 11);
+  private static final Button OPERATOR_PAD_BUTTON_TWELVE = new JoystickButton(OPERATOR_PAD, 12);
+  @SuppressWarnings("unused")
+  private static final Button FORCE_FIRE_BUTTON = new AndJoystickButton(OPERATOR_PAD, 5, OPERATOR_PAD, 7);
+
+  // Operator Control Buttons
+  @SuppressWarnings("unused")
+  private static final JoystickAxisButton OPERATOR_PAD_LEFT_Y_AXIS_UP = new JoystickAxisButton(OPERATOR_PAD,
+      Constants.OPERATOR_PAD_AXIS.OPERATOR_PAD_LEFT_Y_AXIS, JoystickAxisButton.NEGATIVE_ONLY);
+  @SuppressWarnings("unused")
+  private static final JoystickAxisButton OPERATOR_PAD_LEFT_Y_AXIS_DOWN = new JoystickAxisButton(OPERATOR_PAD,
+      Constants.OPERATOR_PAD_AXIS.OPERATOR_PAD_LEFT_Y_AXIS, JoystickAxisButton.POSITIVE_ONLY);
+  @SuppressWarnings("unused")
+  private static final JoystickAxisButton OPERATOR_PAD_RIGHT_Y_AXIS_UP = new JoystickAxisButton(OPERATOR_PAD,
+      Constants.OPERATOR_PAD_AXIS.OPERATOR_PAD_RIGHT_Y_AXIS, JoystickAxisButton.NEGATIVE_ONLY);
+  @SuppressWarnings("unused")
+  private static final JoystickAxisButton OPERATOR_PAD_RIGHT_Y_AXIS_DOWN = new JoystickAxisButton(OPERATOR_PAD,
+      Constants.OPERATOR_PAD_AXIS.OPERATOR_PAD_RIGHT_Y_AXIS, JoystickAxisButton.POSITIVE_ONLY);
 
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
