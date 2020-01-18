dcb82ee15f8178ad240b0ac67a8b025521d2dbc6
Ihedgehogs <58241515+Ihedgehogs@users.noreply.github.com>
2020-01-18 15:05:23 -0500

    added shooter subsystem

---
+++ src/main/java/org/mayheminc/robot2020/subsystems/Shooter.java
@@ -0,0 +1,17 @@
+package org.mayheminc.robot2020.subsystems;
+
+import com.kauailabs.navx.frc.*;
+import org.mayheminc.util.History;
+
+import edu.wpi.first.wpilibj.*;
+import com.ctre.phoenix.motorcontrol.*;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import org.mayheminc.util.MayhemTalonSRX;
+import org.mayheminc.util.Utils;
+
+public class Shooter extends SubsystemBase {
+}
