0d62f142b87fe964153dbf25fc50c33d24ff4c57
Ihedgehogs <58241515+Ihedgehogs@users.noreply.github.com>
2020-01-21 22:42:08 -0500

    asigned buttons

---
+++ src/main/java/org/mayheminc/robot2020/commands/MagazineSetTurntable.java
@@ -0,0 +1,43 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.mayheminc.robot2020.commands;
+
+import org.mayheminc.robot2020.RobotContainer;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class MagazineSetTurntable extends CommandBase {
+  boolean m_speed;
+
+  /**
+   * Creates a new MagazineSetTurntable.
+   */
+  public MagazineSetTurntable(boolean b) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(RobotContainer.magazine);
+    m_speed = b;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    RobotContainer.magazine.setTurntableSpeed(m_speed);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    RobotContainer.magazine.setTurntableSpeed(false);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
