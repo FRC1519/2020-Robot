f6995415f0262680b31380ef84210e3b84ebdb18
Caleb Denio <cjdenio44@gmail.com>
2021-02-16 21:39:22 -0500

    Migrate DriveSlalom to TurnToHeading

--- src/main/java/org/mayheminc/robot2020/autonomousroutines/DriveSlalom.java
+++ src/main/java/org/mayheminc/robot2020/autonomousroutines/DriveSlalom.java
@@ -17,28 +17,39 @@ public class DriveSlalom extends SequentialCommandGroup {
          * Add your docs here.
          */
         public DriveSlalom() {
-                // wut
                 addCommands(new DriveZeroGyro(0.0));
 
-                // drive forward
-                addCommands(new DriveStraightOnHeading(0.3, DistanceUnits.INCHES, 11, 0));
-                // turn
-                addCommands(new DriveStraightOnHeading(0.3, DistanceUnits.INCHES, 78, 295));
-                // drive forward some more
-                addCommands(new DriveStraightOnHeading(0.3, DistanceUnits.INCHES, 12 * 11, 0));
-                // turn again
-                addCommands(new DriveStraightOnHeading(0.3, DistanceUnits.INCHES, 78, 65));
-                // forward
-                addCommands(new DriveStraightOnHeading(0.3, DistanceUnits.INCHES, 48, 0));
-                // turn again again
-                addCommands(new DriveStraightOnHeading(0.3, DistanceUnits.INCHES, 54, 270));
-                addCommands(new DriveStraightOnHeading(0.3, DistanceUnits.INCHES, 54, 185));
-                addCommands(new DriveStraightOnHeading(0.3, DistanceUnits.INCHES, 60, 115));
+                // // drive forward
+                // addCommands(new DriveStraightOnHeading(0.3, DistanceUnits.INCHES, 11, 0));
+                // // turn
+                // addCommands(new DriveStraightOnHeading(0.3, DistanceUnits.INCHES, 78, 295));
+                // // drive forward some more
+                // addCommands(new DriveStraightOnHeading(0.3, DistanceUnits.INCHES, 12 * 11,
+                // 0));
+                // // turn again
+                // addCommands(new DriveStraightOnHeading(0.3, DistanceUnits.INCHES, 78, 65));
+                // // forward
+                // addCommands(new DriveStraightOnHeading(0.3, DistanceUnits.INCHES, 48, 0));
+                // // turn again again
+                // addCommands(new DriveStraightOnHeading(0.3, DistanceUnits.INCHES, 54, 270));
+                // addCommands(new DriveStraightOnHeading(0.3, DistanceUnits.INCHES, 54, 185));
+                // addCommands(new DriveStraightOnHeading(0.3, DistanceUnits.INCHES, 60, 115));
 
-                // lotta driving
-                addCommands(new DriveStraightOnHeading(0.3, DistanceUnits.INCHES, 12 * 12, 180));
-                // turn again again 2 electic boogalo
-                addCommands(new DriveStraightOnHeading(0.3, DistanceUnits.INCHES, 78, 245));
-                addCommands(new DriveStraightOnHeading(0.3, DistanceUnits.INCHES, 36, 180));
+                // // lotta driving
+                // addCommands(new DriveStraightOnHeading(0.3, DistanceUnits.INCHES, 12 * 12,
+                // 180));
+                // // turn again again 2 electic boogalo
+                // addCommands(new DriveStraightOnHeading(0.3, DistanceUnits.INCHES, 78, 245));
+                // addCommands(new DriveStraightOnHeading(0.3, DistanceUnits.INCHES, 36, 180));
+
+                addCommands(new TurnToHeading(20, 0.3, -45, TurnToHeading.Direction.LEFT));
+                addCommands(new DriveStraightOnHeading(0.3, DistanceUnits.INCHES, 15, 295));
+                addCommands(new TurnToHeading(20, 0.3, 0, TurnToHeading.Direction.RIGHT));
+                addCommands(new DriveStraightOnHeading(0.3, DistanceUnits.INCHES, 120, 0));
+                addCommands(new TurnToHeading(20, 0.3, 45, TurnToHeading.Direction.RIGHT));
+                addCommands(new DriveStraightOnHeading(0.3, DistanceUnits.INCHES, 20, 45));
+                addCommands(new WaitCommand(2));
+                addCommands(new TurnToHeading(0, 0.3, -45, TurnToHeading.Direction.LEFT));
+                addCommands(new TurnToHeading(0, 0.3, -135, TurnToHeading.Direction.LEFT));
         }
 }
